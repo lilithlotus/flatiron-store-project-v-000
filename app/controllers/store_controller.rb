18a8524a4c3179e2f43dbb321f623f0199fc8404
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-16 17:24:20 -0500

    Done.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,6 @@ class StoreController < ApplicationController
   def index
     @categories = Category.all
     @items = Item.all
+    @user = User.find_by(id: session[:user_id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
+
+  def self.available_items
+
+  end
+
 end
